aa6a78f1212da2dfd61d3f95adaea707ac5ce9fb
baldan.federico <baldan.federico@TAVE-PC10.OSDBSESTO.local>
2021-04-15 14:10:23 +0200

    es1

--- src/serializzazione/Serializzazione.java
+++ src/serializzazione/Serializzazione.java
@@ -5,6 +5,15 @@
  */
 package serializzazione;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author baldan.federico
@@ -15,7 +24,36 @@ public class Serializzazione {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    Studente st= new Studente("Federico","Baldan",4);
+    
+        try {
+        FileOutputStream fo= new FileOutputStream("Test.ser");
+        ObjectOutputStream os= new ObjectOutputStream(fo);
+        os.writeObject(9);
+        os.writeObject("Hola");
+        os.writeFloat((float) 0.434);
+//        os.writeObject(true);
+//        os.writeObject("a");
+        os.flush();
+        os.close();
+        fo.close();
+         FileInputStream fi= new FileInputStream("Test.ser");
+         ObjectInputStream oi = new ObjectInputStream(fi);
+         int v1= (int)oi.readObject();
+         System.out.println("v1 : "+v1);
+         String v2= (String)oi.readObject();
+         System.out.println("v2 : "+v2);
+         float v3= (float)oi.readObject();
+         System.out.println("v3 : "+v3);
+        } catch (FileNotFoundException ex) {
+            System.out.println("Impossibile trovare il file");
+        }
+        catch (IOException ex) {
+            System.out.println("Non va nulla");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Serializzazione.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
+ 
